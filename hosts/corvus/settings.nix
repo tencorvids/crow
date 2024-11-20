bcd5d25bf4ece087932a2c158999c2a867f4595f
rew <rew@tencorvids.com>
2024-11-20 18:05:27 -0500

    Corvus host

---
+++ hosts/corvus/settings.nix
@@ -0,0 +1,15 @@
+{
+  system = "x86_64-linux";
+  cpu = "intel"; # Options: "intel", "amd"
+  gpu = "intel"; # Options: "nvidia", "intel", "amd"
+
+  username = "rew";
+  timezone = "America/New_York";
+  locale = "en_US.UTF-8";
+  name = "rew";
+  email = "rew@tencorvids.com";
+  homeDir = "/home/rew";
+  stateVersion = "24.05";
+
+  theme = "skull";
+}

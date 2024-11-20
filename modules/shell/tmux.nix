b79c31d0fc4d062051ea2fff1eb3bc355352308c
rew <rew@tencorvids.com>
2024-11-20 11:07:37 -0500

    Tmux prefix remap

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -3,7 +3,8 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   options.tmux.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -14,5 +15,18 @@
     home.packages = with pkgs; [
       tmux
     ];
+
+    programs.tmux = {
+      enable = true;
+
+      # Set the prefix key to Ctrl+a
+      prefix = "C-a";
+
+      # Additional configuration
+      extraConfig = ''
+        # Send prefix using the new key
+        bind C-a send-prefix
+      '';
+    };
   };
 }

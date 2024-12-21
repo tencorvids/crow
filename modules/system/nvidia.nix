5997fbe1dc3b511a2af198d65f6f23bd8575fad3
rew <rew@tencorvids.com>
2024-12-21 13:31:07 -0500

    Move to stable nvidia drivers

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -18,7 +18,7 @@
         nvidiaSettings = true;
         powerManagement.enable = true;
         modesetting.enable = true;
-        package = config.boot.kernelPackages.nvidiaPackages.production;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
       graphics = {
         enable = true;
